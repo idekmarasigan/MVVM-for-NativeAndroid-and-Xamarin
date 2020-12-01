e4fa2d9673cd04e2779b433541b033c2185714e9
idekmarasigan <70684099+idekmarasigan@users.noreply.github.com>
2020-12-01 14:54:23 +0800

    Add files via upload

---
+++ XamarinMvvm/XamarinMvvm/XamarinMvvm/Models/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinMvvm.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Counter { get; set; }
+    }
+}
---
+++ XamarinMvvm/XamarinMvvm/XamarinMvvm/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamarinMvvm.Models;
+
+namespace XamarinMvvm.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+
+        private Animal _animal;
+        public Animal Animal
+        {
+            get => _animal;
+            set => Set(ref _animal, value);
+        }
+
+
+        private int _count;
+        public int Count
+        {
+            get => _count;
+            set => Set(ref _count, value);
+        }
+
+        public ICommand ClickMeCommand => new Command(() =>  this.ClickMe());
+
+        public MainPageViewModel() {
+            Animal = new Animal
+            {
+                Name = "Dog",
+                Counter = 0
+            };
+
+  
+        }
+
+        private void ClickMe()
+        {
+            Count =  Animal.Counter++;
+        }
+    }
+}
